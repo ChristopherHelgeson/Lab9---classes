9439c16b027ae8e6aa79dc63ebf285ac5b909839
Christopher Helgeson <ChristopherHelgeson@hotmail.com>
2017-07-06 16:48:25 -0400

    first commit

---
+++ CircleApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9___classes
+{
+    class CircleApp
+    {
+        static void Main(string[] args)
+        {
+            string playAgain = "y";
+            int iterations = 0;
+            while (playAgain == "y" || playAgain == "Y")
+            {
+                double userInputRadius = 0;
+                while (userInputRadius <= 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("CIRCLE AREA AND CIRCUMFERENCE CALCULATOR");
+                    Console.Write("\nPlease enter a positive value for the radius for your circle: ");
+                    userInputRadius = double.Parse(Console.ReadLine());
+                }
+                Circle userCircle = new Circle(userInputRadius);
+                Console.WriteLine(userCircle.GetFormattedCircumference());
+                Console.WriteLine(userCircle.GetFormattedArea());
+
+                Console.Write("\nAnother circle? (Y/N): ");
+                playAgain = Console.ReadLine();
+                iterations++;
+            }
+            Console.WriteLine("\nGoodbye! Thanks for playing. You created " + iterations + " circles this round.\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9___classes
+{
+    class Circle
+    {
+        private double radius;
+        private double pi = Math.PI;
+        
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetCircumference()
+        {
+            double x = pi * 2 * radius;
+            return x;
+        }
+
+        public string GetFormattedCircumference()
+        {
+            string x = "\nThe circumference of your circle is: " + FormatNumber(GetCircumference());
+            return x;
+        }
+
+        public double GetArea()
+        {
+            double x = pi * radius * radius;
+            return x;
+        }
+
+        public string GetFormattedArea()
+        {
+            string x = "\nThe area of your circle is: " + FormatNumber(GetArea());
+            return x;
+        }
+
+        private string FormatNumber (double x)
+        {
+            x = Math.Round(x, 2, MidpointRounding.AwayFromZero);
+            string y = x.ToString();
+            return y;
+        }
+    }
+}
